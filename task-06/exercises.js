a3a7796d18e0155bb0af763de588f1b09e2f0a15
OlyaPro <volha.prostak@gmail.com>
2023-02-03 10:34:57 +0300

    modified exercises.js

--- task-06/exercises.js
+++ task-06/exercises.js
@@ -1,45 +1,41 @@
 //task 1
-let string = '12232';
-if((+string[0] + +string[1]+ +string[2])===(+string[3] + +string[4])) {
-console.log('Yes');
+let string = "12232";
+if (+string[0] + +string[1] + +string[2] === +string[3] + +string[4]) {
+  console.log("Yes");
 } else {
-console.log('No');
-} 
+  console.log("No");
+}
 
 // task 2
 let n = 1000;
-	i = 0;
-	while (n > 50){
-		n /= 2;
-		i++;
-	}
-	console.log(n);
-	console.log(i);
+let num = 0;
+
+while (n > 50) {
+  n /= 2;
+  num++;
+}
 
 // task 3
-    const arrNumber=[12, 15, 20, 25, 59, 79];
-let total=0;
+const arr = [12, 15, 20, 25, 59, 79];
+let total = 0;
 
-for (let i=0;i<arrNumber.length;i++) {
-  total += arrNumber [i];
+for (let i = 0; i < arr.length; i++) {
+  total += arr[i];
 }
-let avg = total / arrNumber.length;
- console.log("Среднее арифметическое значение=" + avg);
+let avg = total / arr.length;
 
 // task 4
- const arrayNumber=[1, 2, 3, 4, 5];
+const arrayNumber = [1, 2, 3, 4, 5];
 
-arrayNumber.splice(3, 0, 'a','b','c');
-console.log(arrayNumber);
+arrayNumber.splice(3, 0, "a", "b", "c");
 
 // task 5
-const arrayNum=[1, 2, 3, 4, 5];
-arrayNum.splice(1, 0, 'a','b');
-arrayNum.splice(6, 0, 'c');
-arrayNum.splice(8, 0, 'e');
-console.log(arrayNum);
+const arrayNum = [1, 2, 3, 4, 5];
+arrayNum.splice(1, 0, "a", "b");
+arrayNum.splice(6, 0, "c");
+arrayNum.splice(8, 0, "e");
 
 // task 6
-const arrNum=[3, 4, 1, 2, 7];
-console.log(arrNum.sort((a, b) => a - b));
-console.log(arrNum.sort((a, b) => b - a));
+const arrNum = [3, 4, 1, 2, 7];
+arrNum.sort((a, b) => a - b);
+arrNum.sort((a, b) => b - a);
