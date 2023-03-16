677a2034a6b8c46daa0edb70acbbbe08cc4f95e0
OlyaPro <123068123+OlyaPro@users.noreply.github.com>
2023-03-16 21:30:15 +0300

    Update wdioTest.js

--- task-18/test/specs/wdioTest.js
+++ task-18/test/specs/wdioTest.js
@@ -9,21 +9,21 @@ describe('webdriver.io test', () => {
     await browser.closeWindow();
   });
   it('should check that the side menu contains the word "Donate" in the main header "Community"', async () => {
-    await $('div:nth-child(1)>a:nth-child(7)').click();
-    expect(await $(`ul li:nth-child(6)>a`).getText()).to.contain('Donate');
+    await $("//*[@class='navbar__items']/a[position()=6]").click();
+    expect(await $("//*[@href='/community/donate']").getText()).to.contain('Donate');
   });
 
   it('should go to the "Automation Protocols" tab through the search field', async () => {
     await $('.DocSearch-Button-Placeholder').click();
     await $('.DocSearch-Input').setValue('Automation Protocols');
-    await $('li#docsearch-item-0 >a > div').click();
-    expect(await $(`div.theme-doc-markdown.markdown header h1`).getTitle()).to.contain('Automation Protocols');
+    await $("//*[@class='DocSearch-Hit']/a/div  ").click();
+    expect(await $("//h1[text()='Automation Protocols']").getTitle()).to.contain('Automation Protocols');
   });
 
-  it('should check for the presence of the "Apium" subheader of the main header "Protocols" of the side menu', async () => {
-    await $('div:nth-child(1) > a:nth-child(4)').click();
-    await $('ul > li:nth-child(2) > div > a').click();
-    const appiumSubHeader = await $(`li:nth-child(2) ul li:nth-child(1) a`);
+  it("check the presence of the 'Apium' subheader under the 'Protocols' main header in the side menu, displayed upon select the 'API'from the main menu", async () => {
+    await $("//*[@class='navbar__items']/a[@href='/docs/api']").click();
+    await $("//*[contains(@class,'menu__list')]/a[@href='/docs/api/protocols']").click();
+    const appiumSubHeader = await $("//*[@href='/docs/api/appium']");
     expect(await appiumSubHeader.getText()).to.equal('Appium');
   });
 });
