ca8d819340a99b74ef9715b1b7b1b39d10744523
OlyaPro <123068123+OlyaPro@users.noreply.github.com>
2023-03-16 20:46:53 +0300

    Update chromedriver.test.js

--- task-17/test/chromedriver.test.js
+++ task-17/test/chromedriver.test.js
@@ -23,7 +23,7 @@ describe('Chromedriver test', function () {
     });
 
     it('should check that the main title is "Chrome Extensions" with the new highlight', async function () {
-        await driver.findElement(By.css('ul li:nth-child(3) div.PsKE7e.qV4dIc div a')).click();
+        await driver.findElement(By.xpath("//*[@class='VsJjtf']//a[@href='/extensions']")).click();
         const newMainTitle = await driver.findElement(By.css("h1[dir='ltr'] span"));
         await driver.executeScript('arguments[0].style.backgroundColor = "green"', newMainTitle);
         expect(await newMainTitle.getText()).to.equal('Chrome Extensions');
@@ -31,14 +31,14 @@ describe('Chromedriver test', function () {
 
     it('should check that the first link contains the word "driver" when searching for "driver" on the search page', async function () {
         await driver.findElement(By.css("span svg[class='vu8Pwe tCHXDc YSH9J']")).click();
-        await driver.findElement(By.css("div input[type='search']")).sendKeys('driver');
-        await driver.findElement(By.css("span svg[class='vu8Pwe']")).click();
-        const firstLinkText = await driver.findElement(By.css("div:nth-child(1) > div > div.yDWqEe > b"));
+        await driver.findElement(By.css("[type='search']")).sendKeys('driver');
+        await driver.findElement(By.css("[class='vu8Pwe']")).click();
+        const firstLinkText = await driver.findElement(By.xpath("//*[@class='yDWqEe']/b"));
         expect(await firstLinkText.getText()).to.include('driver');
     });
 
     it('should check that the URL contains "/mobile-emulation" in the "Mobile Emulation" tab', async function () {
-        await driver.findElement(By.css(' li.VsJjtf.oXBWEc  div  div a')).click();
+        await driver.findElement(By.xpath("//a[text()='Дополнительно']")).click();
         await driver.findElement(By.linkText('Mobile Emulation')).click();
         let CurrentUrl = await driver.getCurrentUrl();
         expect(CurrentUrl).to.include('/mobile-emulation');
