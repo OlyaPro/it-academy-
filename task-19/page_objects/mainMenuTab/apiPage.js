453df18b1ab3d5435db3207e6cb35d91d5d403cb
OlyaPro <123068123+OlyaPro@users.noreply.github.com>
2023-04-05 12:08:40 +0300

    Update apiPage.js

--- task-19/page_objects/mainMenuTab/apiPage.js
+++ task-19/page_objects/mainMenuTab/apiPage.js
@@ -1,10 +1,7 @@
 const BasePage = require('../basePage');
 const BaseElements = require('../../helpers/baseElements');
-<<<<<<< HEAD
-
-=======
 const topNavigation = require('../generalComponents/topNavigation');
->>>>>>> 0555eaa519a82a3d6074f94b646f65978c1fd4a6
+
 
 
 class ApiPage extends BasePage {
