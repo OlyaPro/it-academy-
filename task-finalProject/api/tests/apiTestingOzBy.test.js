e8d83cc6b5f75ef8b59ca0036ca28e6b212f420f
OlyaPro <Volha.Prostak@gmail.com>
2023-04-26 22:23:03 +0300

    update apiTestingOzBy.test.js

--- task-finalProject/api/tests/apiTestingOzBy.test.js
+++ task-finalProject/api/tests/apiTestingOzBy.test.js
@@ -2,7 +2,8 @@ const axios = require('axios');
 const validator = require('jsonschema');
 const productAddCartSchema = require('../data/productAddCartSchema.v1.json');
 
-describe(`API tests Oz.by`, function () {
+describe(`API tests for Oz.by`, function () {
+    describe('GET request for product data', function () {
     let response;
     beforeAll(async () => {
         response = await axios.get('https://oz.by/goods/ajax/html_box.php', {
@@ -14,23 +15,40 @@ describe(`API tests Oz.by`, function () {
         })
     });
 
-    test('status code should be 200', async () => {
+    test('GET request for Oz.by product details - status code should be 200', async () => {
         expect(response.status).toEqual(200)
     });
 
-    test('json schema should be valid', async () => {
+    test('GET request for Oz.by product details - JSON schema should be valid', async () => {
         const result = await validator.validate(response.data, productAddCartSchema);
         expect(result.valid).toEqual(true)
     });
 
-    test('response.data should have necessary properties of correct type', () => {
-        expect(typeof response.data.status).toBe('boolean');
-        expect(typeof response.data.message).toBe('string');
-        expect(typeof response.data.id_goods).toBe('number');
-        expect(typeof response.data.in_cart).toBe('number');
-        expect(typeof response.data.sgfs_in_cart).toBe('boolean');
-        expect(typeof response.data.type).toBe('string');
-        expect(typeof response.data.orderCashback).toBe('string');
-    });
+    test('GET request for Oz.by product details - response time should not exceed 230ms', async () => {
+        const start = new Date().getTime();
+        await axios.get('https://oz.by/goods/ajax/html_box.php', {
+          params: { idGoods: '101221195', type: 'html' },
+          headers: {
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'X-Requested-With': 'XMLHttpRequest'
+          }
+        });
+        const end = new Date().getTime();
+        const responseTime = end - start;
+        expect(responseTime).toBeLessThanOrEqual(230);
+      });
 
+      describe('POST request for reviews data', function () {
+        test('Test POST request for Oz.by reviews - status code should be 200', async () => {
+          const response = await axios.post('https://oz.by/bowls/more10457032.html', {
+            data: { action: 'moreReview', ajax: '1' },
+            headers: {
+              'Accept': 'application/json, text/javascript, */*; q=0.01',
+              'X-Requested-With': 'XMLHttpRequest'
+            }
+          });
+          expect(response.status).toEqual(200);
+        });
+      });
+    });
 })
