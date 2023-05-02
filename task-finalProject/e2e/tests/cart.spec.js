af8ed3dc1d99cc6f614a1b2b3f4e0712f861afbe
OlyaPro <Volha.Prostak@gmail.com>
2023-05-02 20:36:57 +0300

    update

--- task-finalProject/e2e/tests/cart.spec.js
+++ task-finalProject/e2e/tests/cart.spec.js
@@ -28,50 +28,34 @@ test.describe('test cartPage oz.by website', async function () {
     await mainPage.navigateAndAcceptCookie('https://oz.by/');
     await baseElements.click(topNavigation.formLogin);
     await userForm.loginByEmailAndPass();
-  });
-
-  test('Should add the product to the cart', async () => {
     await topNavigation.searchProducts('Десять негритят');
     await productListing.getProduct('Десять негритят');
     await baseElements.click(productDetails.cart);
     await baseElements.click(topNavigation.shoppingCart);
+  });
+
+  test('Should add the product to the cart', async () => {
     const item = await baseElements.checkTextContent(cart.productInTheCart);
     await expect(item).toContain('Десять негритят');
   });
 
   test('Should change item quantity in the cart', async () => {
-    await topNavigation.searchProducts('Десять негритят');
-    await productListing.getProduct('Десять негритят');
-    await baseElements.click(productDetails.cart);
-    await baseElements.click(topNavigation.shoppingCart);
     const itemQuantityInCart = await cart.changeItemQuantityInCart('2');
     const expectedQuantity = '2';
     await expect(await itemQuantityInCart).toEqual(expectedQuantity);
   });
 
   test("Should check the presence of the 'Confirm and Place Order' form", async () => {
-    await topNavigation.searchProducts('Десять негритят');
-    await productListing.getProduct('Десять негритят');
-    await baseElements.click(productDetails.cart);
-    await baseElements.click(topNavigation.shoppingCart);
     const ConfirmAndPlaceOrder = await baseElements.getText(cart.confirmAndPlaceOrder);
     await expect(ConfirmAndPlaceOrder).toBeTruthy();
   });
 
   test("Should check the presence bonus account in the 'Confirm and Place Order' form", async () => {
-    await topNavigation.searchProducts('Десять негритят');
-    await productListing.getProduct('Десять негритят');
-    await baseElements.click(productDetails.cart);
-    await baseElements.click(topNavigation.shoppingCart);
     const bonusAccount = await baseElements.checkTextContent(cart.bonusAccount);
     await expect(bonusAccount).toContain('Бонусный счёт');
   });
 
   test('Should remove all item from cart', async () => {
-    await topNavigation.searchProducts('Десять негритят');
-    await productListing.getProduct('Десять негритят');
-    await baseElements.click(productDetails.cart);
-    await baseElements.click(topNavigation.shoppingCart);
     const pageAfterDeleteInfo = await cart.removeItemFromCart();
     await expect(pageAfterDeleteInfo).toContain('Чтобы найти товары, используйте поиск');
   });
